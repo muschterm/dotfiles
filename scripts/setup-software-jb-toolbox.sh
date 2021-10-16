898da2aabcf229f376e79c5796505ce07ea8153d
Matthew Muschter <muschterm@linux.com>
2021-10-16 11:08:57 -0400

    Update Jetbrains Toolbox to support ARM mac.

--- scripts/setup-software-jb-toolbox.sh
+++ scripts/setup-software-jb-toolbox.sh
@@ -2,7 +2,7 @@
 
 : ${DF_SETUP_JB_TOOLBOX:="false"}
 if [ "$DF_SETUP_JB_TOOLBOX" = "true" ]; then
-	: ${DF_JB_TOOLBOX_VERSION:="1.20.8352"}
+	: ${DF_JB_TOOLBOX_VERSION:="1.21.9712"}
 
 	(
 		if [ ! -f "$DF_APP_HOME/jetbrains-toolbox" ] && [ ! -d "$DF_APP_HOME/JetBrains Toolbox.app" ]; then
@@ -10,20 +10,21 @@ if [ "$DF_SETUP_JB_TOOLBOX" = "true" ]; then
 			Installing JetBrains Toolbox (version $DF_JB_TOOLBOX_VERSION)...
 			HERE
 
-			download_url=
-			saved_download_location=
-
-			local_jb_toolbox_download_url="https://download.jetbrains.com/toolbox"
+			suffix=
 			if [ "$DF_OS_WINDOWS" = "true" ]; then
-				download_url="$local_jb_toolbox_download_url/jetbrains-toolbox-${DF_JB_TOOLBOX_VERSION}.win.zip"
-				saved_download_location="$DF_DOWNLOADS_HOME/jetbrains-toolbox-${DF_JB_TOOLBOX_VERSION}.win.zip"
+				suffix=".win.zip"
 			elif [ "$DF_OS_LINUX" = "true" ]; then
-				download_url="$local_jb_toolbox_download_url/jetbrains-toolbox-${DF_JB_TOOLBOX_VERSION}.tar.gz"
-				saved_download_location="$DF_DOWNLOADS_HOME/jetbrains-toolbox-${DF_JB_TOOLBOX_VERSION}.tar.gz"
+				suffix=".tar.gz"
 			elif [ "$DF_OS_MACOS" = "true" ]; then
-				download_url="$local_jb_toolbox_download_url/jetbrains-toolbox-${DF_JB_TOOLBOX_VERSION}.dmg"
-				saved_download_location="$DF_DOWNLOADS_HOME/jetbrains-toolbox-${DF_JB_TOOLBOX_VERSION}.dmg"
+				if [ "$DF_ARCH_ARM_64" = "true" ]; then
+					suffix="-arm64.dmg"
+				else
+					suffix=".dmg"	
+				fi
 			fi
+
+			download_url="https://download.jetbrains.com/toolbox/jetbrains-toolbox-${DF_JB_TOOLBOX_VERSION}${suffix}"
+			saved_download_location=$DF_DOWNLOADS_HOME/jetbrains-toolbox-${DF_JB_TOOLBOX_VERSION}${suffix}
 
 			user-install-software --home "$DF_APP_HOME" --tar-args "--strip-components=1" --dmg-vol "JetBrains Toolbox" --dmg-app "JetBrains Toolbox.app" "$download_url" "$saved_download_location"
 		fi
