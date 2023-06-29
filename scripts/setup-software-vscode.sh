d63cbd5768baaaccee29f423d62e992bd928fcdf
Matthew Muschter <muschterm@linux.com>
2023-06-29 13:18:55 -0400

    Fix VSCode command line setup for MacOS.

--- scripts/setup-software-vscode.sh
+++ scripts/setup-software-vscode.sh
@@ -2,7 +2,7 @@
 
 : ${DF_SETUP_VSCODE:="false"}
 if [ "${DF_SETUP_VSCODE}" = "true" ]; then
-	if [ "$DF_OS_MACOS" = "true" ]; then
+	if [ "$DF_OS" = "$DF_OS_MACOS" ]; then
 		local_vscode_app="$DF_APP_HOME/Visual Studio Code.app"
 		[ -d "${local_vscode_app}/Contents" ] && export PATH="${local_vscode_app}/Contents/Resources/app/bin:$PATH"
 		unset local_vscode_app
