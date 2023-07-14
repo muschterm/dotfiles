2a5b39e818b3d410bf07b9ae256465bf72f7d9c6
Matthew Muschter <muschterm@linux.com>
2023-07-14 14:05:20 -0400

    Remove custom nodejs prefix.

--- scripts/setup-software-nodejs.sh
+++ scripts/setup-software-nodejs.sh
@@ -41,13 +41,13 @@ if [ "$DF_SETUP_NODEJS" = "true" ]; then
 fi
 
 # npm config setup
-if command -v npm > /dev/null; then
-	local_npm_prefix="${HOME}/.npm-packages"
-	if [ -f "${HOME}/.npmrc"  ] && [ "$(cat "${HOME}/.npmrc" | grep "${local_npm_prefix}")" != "prefix=${local_npm_prefix}" ]; then
-		npm config set prefix "${local_npm_prefix}"
-	fi
+# if command -v npm > /dev/null; then
+# 	local_npm_prefix="${HOME}/.npm-packages"
+# 	if [ -f "${HOME}/.npmrc"  ] && [ "$(cat "${HOME}/.npmrc" | grep "${local_npm_prefix}")" != "prefix=${local_npm_prefix}" ]; then
+# 		npm config set prefix "${local_npm_prefix}"
+# 	fi
 
-	export PATH="$local_npm_prefix/bin:$PATH"
+# 	export PATH="$local_npm_prefix/bin:$PATH"
 
-	unset local_npm_prefix
-fi
+# 	unset local_npm_prefix
+# fi
