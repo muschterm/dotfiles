ea7e90b5bfb221a7fce1896a6cda888089140773
Matthew Muschter <muschterm@linux.com>
2024-01-14 13:52:10 -0500

    Update the docker compose install script along with minor updates:
    - Add example .zshenv and .zshrc files
    - Update NodeJS version

--- scripts/install-docker-compose-cli.sh
+++ scripts/install-docker-compose-cli.sh
@@ -1,11 +1,38 @@
 #!/usr/bin/env zsh
 
-if [ "$(whoami)" != "root" ]; then
-	echo Please run this script using sudo or as root.
+if command -v apt >/dev/null; then
+	if docker compose version &>/dev/null; then
+		printf -- "Docker Compose already installed.\n\n"
+
+		docker compose version
+
+		cat <<-HERE
+
+			Update using APT:
+
+			  $([ "$(whoami)" != "root" ] && printf -- "sudo ")apt update
+			  $([ "$(whoami)" != "root" ] && printf -- "sudo ")apt upgrade
+		HERE
+	else
+		cat <<-HERE
+			Please install Docker Compose using APT:
+
+			  $([ "$(whoami)" != "root" ] && printf -- "sudo ")apt install docker-compose-plugin
+		HERE
+	fi
+
 	exit
 fi
 
-: ${VERSION:="2.23.3"}
+: ${DOCKER_CONFIG:="$HOME/.docker"}
+
+if [ "$(whoami)" = "root" ]; then
+	DOCKER_CONFIG="/usr/local/lib/docker"
+fi
+
+mkdir -p "$DOCKER_CONFIG/cli-plugins"
+
+: ${DOCKER_COMPOSE_VERSION:="2.24.0"}
 
 if [ "$(print-distro)" = "alpine" ]; then
 	cat <<-HERE
@@ -15,7 +42,7 @@ if [ "$(print-distro)" = "alpine" ]; then
 fi
 
 if command -v curl >/dev/null; then
-	curl -L "https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o $DOCKER_CONFIG/cli-plugins/docker-compose
 else
 	printf -- "Cannot install docker-compose - missing curl.\n"
 fi
