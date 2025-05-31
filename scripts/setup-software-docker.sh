d89174cae87d8b0e1e2e25c504c03ae4c5ab3046
Matthew Muschter <muschterm@linux.com>
2025-05-31 17:52:45 -0400

    Fix docker configuration.

--- scripts/setup-software-docker.sh
+++ scripts/setup-software-docker.sh
@@ -4,11 +4,11 @@ if [ "${DF_SETUP_DOCKER}" = "true" ]; then
 	if [ -n "$ZSH_VERSION" ]; then
 		if [ ! -f "$HOME/.docker/completions/_docker" ]; then
 			mkdir -p "$HOME/.docker/completions"
-			docker completions zsh > "$HOME/.docker/completions/_docker"
+			docker completion zsh > "$HOME/.docker/completions/_docker"
 
 			cat <<-HERE
 				# after upgrading docker, run to ensure zsh completions are accurate:
-				docker completions zsh > "$HOME/.docker/completions/_docker"
+				docker completion zsh > "$HOME/.docker/completions/_docker"
 			HERE
 		fi
 
@@ -16,11 +16,11 @@ if [ "${DF_SETUP_DOCKER}" = "true" ]; then
 	elif [ -n "$BASH_VERSION" ]; then
 		if [ ! -f "$HOME/.local/share/bash-completion/completions/docker" ]; then
 			mkdir -p "$HOME/.local/share/bash-completion/completions"
-			docker completions bash > "$HOME/.local/share/bash-completion/completions/docker"
+			docker completion bash > "$HOME/.local/share/bash-completion/completions/docker"
 
 			cat <<-HERE
 				# after upgrading docker, run to ensure bash completions are accurate:
-				docker completions bash > "$HOME/.local/share/bash-completion/completions/docker"
+				docker completion bash > "$HOME/.local/share/bash-completion/completions/docker"
 			HERE
 
 		fi
